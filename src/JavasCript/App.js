c45c660e8c552184a89f8605030e82f1249431f7
Hélio Carlitos <112761333+heliocarlitos@users.noreply.github.com>
2024-08-22 23:38:25 +0200

    Actualizado

---
+++ src/JavasCript/App.js
@@ -0,0 +1,12 @@
+// Pega o Menu
+const MenuMobile = document.getElementById('MenuMobile');
+
+// Esta função deixa o Menu visivel
+function MostrarMenuMobile() {
+    MenuMobile.style.display = 'flex'
+}
+
+// Esta função oculta o Menu
+function FecharMenuMobile() {
+    MenuMobile.style.display = 'none'
+}
